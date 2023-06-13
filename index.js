d8d5a6073a047c28d2fd79bc26b860c580476b51
Caleb Kiprop Rono <ckiprop14@gmail.com>
2023-06-13 12:35:34 +0300

    Assignment done

---
+++ index.js
@@ -0,0 +1,19 @@
+let num1 = 2000
+let num2 = 100
+const divide = () => {
+    return 2000 / 100
+}
+
+
+
+let x = 2
+const square = (x) => {
+    return x * x
+}
+square(x)
+let a = 3
+let b = 4
+const add = (a, b) => {
+    return a + b
+}
+add(a, b)
